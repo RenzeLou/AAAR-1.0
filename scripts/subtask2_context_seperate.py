83f880d8a68ebe503889484a91ed5fb6eddbdb5d
Renze Lou <rml6079@psu.edu>
2024-04-07 00:14:07 -0400

    uodate subtask2 processs: now can process papers with multiple .tex files

--- scripts/subtask2_context_seperate.py
+++ scripts/subtask2_context_seperate.py
@@ -134,6 +134,87 @@ def find_expriment_location(tex_content):
     return locations
 
 
+def insert_other_sections(main_tex_content, content_list):
+    combined_tex = []
+    for line in main_tex_content:
+        if re.search(r"\\input\{.*\}", line):
+            # find the file name in this line
+            # for example, sections/5ExperimentalSetup or 5ExperimentalSetup
+            # i.e., the string inside the \input{}
+            f_name = re.search(r"\\input\{(.*)\}", line).group(1)
+            # for sections/5ExperimentalSetup, we need to remove the "sections/" part
+            f_name = f_name.split("/")[-1]
+            # make sure f_name is a .tex file
+            f_name = f_name + ".tex" if not f_name.endswith(".tex") else f_name
+            
+            # find this file in the content_list
+            found = False
+            for file_name, content in content_list:
+                if file_name == f_name:
+                    combined_tex.extend(content)
+                    found = True
+                    break
+                
+            if not found:
+                # just add the oringinal line
+                combined_tex.append(line)
+        else:
+            combined_tex.append(line)
+
+    return combined_tex
+    
+    
+def combine_tex(tex_files:list):
+    '''
+    combine multiple tex files into one
+    '''
+    # first find the main.tex (usually has "\documentclass")
+    content_list = []
+    main_idx = -1
+    main_tex_content = None
+    for i, file in enumerate(tex_files):
+        content_lines = red_tex(file)
+        if content_lines is None:
+            continue
+        file_name = os.path.basename(file)
+        content_list.append((file_name, content_lines))  # file_name, file_content
+        for line in content_lines:
+            # if this line contains "\documentclass", then this is the main.tex
+            if re.search(r"\\documentclass", line):
+                if main_tex_content is not None:
+                    # if we already find the main.tex, but there is another main.tex, then raise an error
+                    return None, "multiple main.tex files (have \\documentclass) founded in one project"
+                main_idx = i
+                main_tex_content = content_lines
+                # then delete this file from the content_list
+                # delete the last element
+                # content_list.pop()
+                break
+    if main_tex_content is None:
+        # if there is no main.tex file, then raise an error
+        return None, "no main.tex file founded in this project"
+    
+    
+    # since the other tex files might also have "\input{}" to refer to other tex files
+    # we iterate all the tex files in the content_list, try to make sure all the other tex files are also self-complete
+    ITER_NUM = 3  # empirically set to 3
+    for _ in range(ITER_NUM):
+        # since only do it once will still result in some missing files, we try multiple times
+        new_content_list = []
+        for current_tex in content_list:
+            current_tex_name, current_tex_content = current_tex
+            combined_current_tex = insert_other_sections(current_tex_content, content_list)
+            # replace the current_tex_content with the combined_current_tex
+            new_content_list.append((current_tex_name, combined_current_tex))
+        content_list = new_content_list
+    
+    # combine all the tex files into one, based on the main.tex
+    # for example, if the main.tex has a line "\input{sections/5ExperimentalSetup}" or "\input{5ExperimentalSetup}", then try to find this file in the content_list
+    # if not found, just skip this line
+    combined_tex = insert_other_sections(main_tex_content, content_list)
+            
+    return combined_tex, "success"
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--root_dir", type=str, default="./acl_papers", help="the directory to save the downloaded papers.")
@@ -175,13 +256,21 @@ def main():
                 if file.endswith(".tex"):
                     tex_files.append(os.path.join(root, file))
         
-        if len(tex_files) > 1:
-            # TODO: here, simply skip those papers with multiple .tex files ==> only process those simple structure paper projects
-            continue
-        elif len(tex_files) == 0:
+        tex_content = None
+        
+        if len(tex_files) == 0:
             # TODO: why there is no .tex file in this paper?
             continue
+        elif len(tex_files) > 1:
+            # try to combine multiple tex files into one
+            combined_tex, e_message = combine_tex(tex_files)
+            if combined_tex is None:
+                continue
+            else:
+                # print("==> {} paper need to combine multiple tex files".format(subfolder))
+                tex_content = combined_tex  # combined_tex is a list of lines
         else:
+            # only one tex file
             main_tex = tex_files[0]
             # read this tex file
             # with open(main_tex, "r") as f:
@@ -190,45 +279,45 @@ def main():
             if tex_content is None:
                 # if the tex file cannot be read, skip this paper
                 continue
-            
-            clean_tex = tex_cleaning(tex_content)
-            locations = find_expriment_location(clean_tex)
-            if len(locations) == 0:
-                # mean there is no "experiment" section in this paper, skip this paper
-                continue
-            suitable_paper_num += 1
-            
-            # seperate the context before and after the "experiment" section
-            loc = locations[0]  # TODO: currently only use the first "experiment" section
-            context_before_exp = clean_tex[:loc]
-            context_after_exp = clean_tex[loc:]
-            save_content = {
-                "context_before_exp": context_before_exp,
-                "context_after_exp": context_after_exp
-            }
-           
-            target_path = os.path.join(args.target_dir, subfolder)
-            os.makedirs(target_path, exist_ok=True)
-            # save the extracted equations of this paper
-            with open(os.path.join(target_path, "context_before_after_exp.json"), "w") as f:
-                json.dump(save_content, f, indent=4)
-            # meanwhile, copy the meta file to the target directory, if meta file exists
-            if os.path.exists(meta_path):
-                shutil.copy(meta_path, os.path.join(target_path, f"{subfolder}_metadata.json"))
-            # also copy the pdf file to the target directory, if pdf file exists
-            pdf_file = os.path.join(args.root_dir, subfolder, f"{subfolder}.pdf")
-            if os.path.exists(pdf_file):
-                shutil.copy(pdf_file, os.path.join(target_path, f"{subfolder}.pdf"))
-            # also save the cleaned tex content
-            with open(os.path.join(target_path, "cleaned_tex.tex"), "w") as f:
-                f.writelines(clean_tex)
-            
-            final_used_paper += 1
+        
+        clean_tex = tex_cleaning(tex_content)
+        locations = find_expriment_location(clean_tex)
+        if len(locations) == 0:
+            # mean there is no "experiment" section in this paper, skip this paper
+            continue
+        suitable_paper_num += 1
+        
+        # seperate the context before and after the "experiment" section
+        loc = locations[0]  # TODO: currently only use the first "experiment" section
+        context_before_exp = clean_tex[:loc]
+        context_after_exp = clean_tex[loc:]
+        save_content = {
+            "context_before_exp": context_before_exp,
+            "context_after_exp": context_after_exp
+        }
+        
+        target_path = os.path.join(args.target_dir, subfolder)
+        os.makedirs(target_path, exist_ok=True)
+        # save the extracted equations of this paper
+        with open(os.path.join(target_path, "context_before_after_exp.json"), "w") as f:
+            json.dump(save_content, f, indent=4)
+        # meanwhile, copy the meta file to the target directory, if meta file exists
+        if os.path.exists(meta_path):
+            shutil.copy(meta_path, os.path.join(target_path, f"{subfolder}_metadata.json"))
+        # also copy the pdf file to the target directory, if pdf file exists
+        pdf_file = os.path.join(args.root_dir, subfolder, f"{subfolder}.pdf")
+        if os.path.exists(pdf_file):
+            shutil.copy(pdf_file, os.path.join(target_path, f"{subfolder}.pdf"))
+        # also save the cleaned tex content
+        with open(os.path.join(target_path, "cleaned_tex.tex"), "w") as f:
+            f.writelines(clean_tex)
+        
+        final_used_paper += 1
             
     
     print("="*50)
     print("Totally {} papers under {}".format(total_paper, args.root_dir))
-    print("Among them, {} papers are suitable for this task (exclude no tex paper and multi tex paper)".format(suitable_paper_num))
+    print("Among them, {} papers are suitable for this task (exclude no tex paper)".format(suitable_paper_num))
     print("Finally used {} papers to extract equations and generate instances (some has no experiments section)".format(final_used_paper))
     print("="*50)
     print("Data are saved under {}".format(args.target_dir))
